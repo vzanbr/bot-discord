c53b7859e36b429d3c131a3b1c603715dba05068
vzanbr <128941678+vzanbr@users.noreply.github.com>
2024-03-10 21:32:29 -0300

    feature : Implementando metodos para o bot

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { Client, GatewayIntentBits } from "discord.js";
 import { config } from "dotenv";
 import { google } from "googleapis";
+import { schedule } from "node-cron";
 
 config();
 
@@ -17,3 +18,33 @@ const youtubeClient = google.youtube({
 })
 
 let lastVideoId = ''
+
+discordClient.login(process.env.DISCORD_TOKEN)
+
+discordClient.on('ready', () => {
+    console.log('Bot online, logado como : ${discordClient.user.tag}')
+    checkNewVideos()
+    schedule("0 * * *", checkNewVideos)
+})
+
+async function checkNewVideos() {
+    try {
+        const response = await youtubeClient.search.list({
+            channelId: UCbtAlJgX6iZwAP5S3vrwkiw,
+            order: "date",
+            part: "snippet",
+            maxResults: 1
+        }).then(res => res)
+        const lastVideo = response.data.items[0]
+        if(lastVideo?.id?.videoId != lastVideoId) {
+            lastVideoId = lastVideo?.id?.videoId
+            const videoUrl = "url do video, {lastVideo}"
+            const message = "confira o ultimo video"
+            const channel = discordClient.channels.cache.get(UCbtAlJgX6iZwAP5S3vrwkiw)
+            channel.send(message + videoUrl)
+        }
+    } catch (error) {
+        console.log( "Erro ao buscar video ")
+        console.log(error)
+    }
+}
